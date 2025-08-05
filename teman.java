c62faeb201665e966c2df14214488caaef820dd5
varazianavsa <varazia_prasetyo_34rpl@student.smktelkom-mlg.sch.id>
2025-08-05 15:07:44 +0700

    Add files via upload

---
+++ teman.java
@@ -0,0 +1,55 @@
+public class teman {
+    public static void main(String[] args) {
+        String nmlengkaptmn1 = "faiza naila azizah";
+        String sekolah1 = "MTSN 1 Malang";
+        int umur1 = 15;
+        char jk1 = 'P';
+
+        System.out.println(nmlengkaptmn1);
+        System.out.println(sekolah1);
+        System.out.println(umur1 + "tahun");
+        System.out.println("jenis kelamin" + jk1);
+
+        String nmlengkaptmn2 = "naila putri andani";
+        String sekolah2 = "MTSN 3 tulungagung";
+        int umur2 = 15;
+        char jk2 = 'P';
+
+        System.out.println(nmlengkaptmn2);
+        System.out.println(sekolah2);
+        System.out.println(umur2 + "tahun");
+        System.out.println("jenis kelamin" + jk2);
+
+        String nmlengkaptmn3 = "dwi nuaraini juwita";
+        String sekolah3 = "SMPN 2 Ngancar";
+        int umur3 = 15;
+        char jk3 = 'P';
+
+        System.out.println(nmlengkaptmn3);
+        System.out.println(sekolah3);
+        System.out.println(umur3 + "tahun");
+        System.out.println("jenis kelamin" + jk3);
+
+        String nmlengkaptmn4 = "putri nayshila";
+        String sekolah4 = "SMP Modern al rifaie 2";
+        int umur4 = 15;
+        char jk4 = 'P';
+
+        System.out.println(nmlengkaptmn4);
+        System.out.println(sekolah4);
+        System.out.println(umur4 + "tahun");
+        System.out.println("jenis kelamin" + jk4);
+
+        String nmlengkaptmn5 = "christabel yemima";
+        String sekolah5 = "SMPN 16 Malang";
+        int umur5 = 15;
+        char jk5 = 'P';
+
+        System.out.println(nmlengkaptmn5);
+        System.out.println(sekolah5);
+        System.out.println(umur5 + "tahun");
+        System.out.println("jenis kelamin" + jk5);
+
+    }
+
+}
